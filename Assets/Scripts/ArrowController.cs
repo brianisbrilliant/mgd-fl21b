2b579294e51836a8bd574d0bb70f441080620adf
Brian Foster <brianisbrilliant@gmail.com>
2021-11-02 13:16:51 -0500

    forced height of direction arrow to be 0.1

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -6,6 +6,8 @@ public class ArrowController : MonoBehaviour
 {
     public PlayerMovement player;
 
+    public float heightAboveGround = 0.1f;
+
     Vector3 arrowScale;             // reference "size" for the arrow
 
     void Start() {
@@ -18,5 +20,11 @@ public class ArrowController : MonoBehaviour
         this.transform.rotation = Quaternion.LookRotation(player.dir, Vector3.up);
         arrowScale.z = player.dir.magnitude;        // update the depth scale based on the strength of our movement
         this.transform.localScale = arrowScale;     // assign the scale to the arrow.
+        if(this.transform.position.y != heightAboveGround) {
+            Vector3 newPos = new Vector3(transform.position.x,
+                                        heightAboveGround,
+                                        transform.position.z);
+            this.transform.position = newPos;
+        }
     }
 }
